30c8e3113baaf81e9846adc5196778f344f850fc
TamsinT <tamsintrickett@hotmail.com>
2015-09-30 19:44:21 +1000

    Pseudo Code and adding list
    Added each input into my lists
    used the buttons to clear the list.
    used a count to count the number of list items

--- js/index.js
+++ js/index.js
@@ -4,6 +4,30 @@ $('document').ready(function(){
         e.preventDefault();
         console.log('f')
 
-          $('#todolist').append( $("#entry").val() );
+          $('#todolist').append('<li>' + $("#entry").val() + '</li>' );
+
+          $('#entry').val(''); /* clears the entry feild after input has been submitted*/
+
+           var count = $('#todolist li').length;
+          $('#taskcount').html(count);
+
       });
+
+      $('#cleartasks').click(function(){
+
+          $('#todolist').html('');
+          $('#taskcount').html('0');
+      });
+
+      /*Psuedo Code
+     I need to take the input (id of entry) saved from the form into a list and keep it there. Adding each input value underneath the
+     last one. To do this i need to add li tags into my fucntion before and after the $('$entry').val()
+
+     I am also using the .length fuction to return the array of the todolist's li's
+
+     then on click of each inputted value and "show" the options "edit" and "remove"
+
+
+
+       */
 });
